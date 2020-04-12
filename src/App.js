6a2f7acdbdfd1c044da150ed4d9e33ec9e230060
Ahmed Abdullah <ahmed@sudofy.com>
2020-04-12 20:34:38 +0500

    attachment viewer integrated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import AttachmentViewer from './components/attachment-viewer/AttachmentViewer';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [attachmentToView, setAttachmentToView] = useState(null);
+
+  const exampleAttachments = [{
+    uploader: {
+      username: 'John Doe',
+      userImage: 'https://themes.themewaves.com/nuzi/wp-content/uploads/sites/4/2013/05/Team-Member-3.jpg'
+    },
+    name: 'Image 1.jpeg',
+    link: 'https://themes.themewaves.com/nuzi/wp-content/uploads/sites/4/2013/05/Team-Member-3.jpg',
+    createdAt: Date.now(),
+    mimetype: 'image/jpeg'
+  }]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {attachmentToView
+        ? <AttachmentViewer
+          attachment={attachmentToView}
+          onClose={() => setAttachmentToView(null)} />
+        : null}
+      {exampleAttachments.map(attachment => <button onClick={() => setAttachmentToView(attachment)}>
+        Image 1
+      </button>)}
     </div>
   );
-}
+};
 
 export default App;
