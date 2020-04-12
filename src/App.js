71b461922e39b8e21f12fa7757af7afea80f7502
Ahmed Abdullah <ahmed@sudofy.com>
2020-04-12 21:43:13 +0500

    Landing page minimal styling

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
 import AttachmentViewer from './components/attachment-viewer/AttachmentViewer';
+import sampleAttachments from './utils/sampleAttachments';
 import './App.css';
 
 const App = () => {
   const [attachmentToView, setAttachmentToView] = useState(null);
 
-  const exampleAttachments = [{
-    uploader: {
-      username: 'John Doe',
-      userImage: 'https://themes.themewaves.com/nuzi/wp-content/uploads/sites/4/2013/05/Team-Member-3.jpg'
-    },
-    name: 'Image 1.jpeg',
-    link: 'https://themes.themewaves.com/nuzi/wp-content/uploads/sites/4/2013/05/Team-Member-3.jpg',
-    createdAt: Date.now(),
-    mimetype: 'image/jpeg'
-  }]
   return (
     <div className="App">
-      {attachmentToView
-        ? <AttachmentViewer
-          attachment={attachmentToView}
-          onClose={() => setAttachmentToView(null)} />
-        : null}
-      {exampleAttachments.map(attachment => <button onClick={() => setAttachmentToView(attachment)}>
-        Image 1
-      </button>)}
+      <p className="Heading">Welcome to Demo of Attachment Viewer</p>
+      <div className="Container">
+        {attachmentToView
+          ? <AttachmentViewer
+            attachment={attachmentToView}
+            onClose={() => setAttachmentToView(null)} />
+          : null}
+        {sampleAttachments.map(attachment => (
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={() => setAttachmentToView(attachment)}>
+            {attachment.name.slice(0, attachment.name.lastIndexOf('.'))}
+          </Button>))}
+      </div>
     </div>
   );
 };
---
+++ src/utils/sampleAttachments.js
@@ -0,0 +1,75 @@
+export default [
+  {
+    uploader: {
+      username: 'John Doe',
+      userImage: 'https://themes.themewaves.com/nuzi/wp-content/uploads/sites/4/2013/05/Team-Member-3.jpg'
+    },
+    name: 'Image 1.jpeg',
+    link: 'https://drive.google.com/open?id=1s1gfKnoIiw1NPbOQ_eS3ZNpce0_TtF30',
+    createdAt: Date.now(),
+    mimetype: 'image/jpeg'
+  }, {
+    uploader: {
+      username: 'John Doe',
+      userImage: 'https://themes.themewaves.com/nuzi/wp-content/uploads/sites/4/2013/05/Team-Member-3.jpg'
+    },
+    name: 'Image 2.jpeg',
+    link: 'https://themes.themewaves.com/nuzi/wp-content/uploads/sites/4/2013/05/Team-Member-3.jpg',
+    createdAt: Date.now(),
+    mimetype: 'image/jpeg'
+  }, {
+    uploader: {
+      username: 'John Doe',
+      userImage: 'https://themes.themewaves.com/nuzi/wp-content/uploads/sites/4/2013/05/Team-Member-3.jpg'
+    },
+    name: 'Image 3.jpeg',
+    link: 'https://themes.themewaves.com/nuzi/wp-content/uploads/sites/4/2013/05/Team-Member-3.jpg',
+    createdAt: Date.now(),
+    mimetype: 'image/jpeg'
+  }, {
+    uploader: {
+      username: 'John Doe',
+      userImage: 'https://themes.themewaves.com/nuzi/wp-content/uploads/sites/4/2013/05/Team-Member-3.jpg'
+    },
+    name: 'Video.mp4',
+    link: 'https://themes.themewaves.com/nuzi/wp-content/uploads/sites/4/2013/05/Team-Member-3.jpg',
+    createdAt: Date.now(),
+    mimetype: 'video/mp4'
+  }, {
+    uploader: {
+      username: 'John Doe',
+      userImage: 'https://themes.themewaves.com/nuzi/wp-content/uploads/sites/4/2013/05/Team-Member-3.jpg'
+    },
+    name: 'Sample PDF.pdf',
+    link: 'https://themes.themewaves.com/nuzi/wp-content/uploads/sites/4/2013/05/Team-Member-3.jpg',
+    createdAt: Date.now(),
+    mimetype: 'application/pdf'
+  }, {
+    uploader: {
+      username: 'John Doe',
+      userImage: 'https://themes.themewaves.com/nuzi/wp-content/uploads/sites/4/2013/05/Team-Member-3.jpg'
+    },
+    name: 'Sample Document.docx',
+    link: 'https://themes.themewaves.com/nuzi/wp-content/uploads/sites/4/2013/05/Team-Member-3.jpg',
+    createdAt: Date.now(),
+    mimetype: 'document/docx'
+  }, {
+    uploader: {
+      username: 'John Doe',
+      userImage: 'https://themes.themewaves.com/nuzi/wp-content/uploads/sites/4/2013/05/Team-Member-3.jpg'
+    },
+    name: 'Sample Presentation.pptx',
+    link: 'https://themes.themewaves.com/nuzi/wp-content/uploads/sites/4/2013/05/Team-Member-3.jpg',
+    createdAt: Date.now(),
+    mimetype: 'document/pptx'
+  }, {
+    uploader: {
+      username: 'John Doe',
+      userImage: 'https://themes.themewaves.com/nuzi/wp-content/uploads/sites/4/2013/05/Team-Member-3.jpg'
+    },
+    name: 'Sample Sheet.xlsx',
+    link: 'https://themes.themewaves.com/nuzi/wp-content/uploads/sites/4/2013/05/Team-Member-3.jpg',
+    createdAt: Date.now(),
+    mimetype: 'document/xlsx'
+  },
+];
