bea85b1f37e277339c2d303e61f21247cf75552d
Ahmed067Abdullah <ahmed@sudofy.com>
2020-05-29 23:25:51 +0500

    Updates

---
+++ precache-manifest.8f783dc51bc110e04dd953f8aab82558.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13b9a53380894b1a4cf93e28095b967e",
+    "url": "/attachment-viewer-React/index.html"
+  },
+  {
+    "revision": "ce90ec64f5669488ead9",
+    "url": "/attachment-viewer-React/static/css/main.2c72a854.chunk.css"
+  },
+  {
+    "revision": "08281ac9bae3924d2062",
+    "url": "/attachment-viewer-React/static/js/2.49b4f782.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/attachment-viewer-React/static/js/2.49b4f782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce90ec64f5669488ead9",
+    "url": "/attachment-viewer-React/static/js/main.e67e2c89.chunk.js"
+  },
+  {
+    "revision": "8d2ad19807127aae5e58",
+    "url": "/attachment-viewer-React/static/js/runtime-main.981a9069.js"
+  },
+  {
+    "revision": "68f8a8190ddc27b55e0fb60a23f21a69",
+    "url": "/attachment-viewer-React/static/media/Sample PDF.68f8a819.pdf"
+  },
+  {
+    "revision": "9640587c085995b34bc06e9536d8f87f",
+    "url": "/attachment-viewer-React/static/media/Sample Videos _ Dummy Videos For Demo Use.9640587c.mp4"
+  }
+]);
